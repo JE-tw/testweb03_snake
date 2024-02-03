72d683d3023376639b95f1678032b1790cd63f46
JE-tw <125960364+JE-tw@users.noreply.github.com>
2024-02-03 18:16:07 +0800

    game over, score, highestscore

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ let ax = 0,
   ay = 0; //蘋果座標
 let direction = "Right"; //蛇前進方向，預設往右
 
+let score = 0; //紀錄分數
+document.getElementById("game_score").innerHTML = "遊戲分數:" + score;
+let highestScore; //紀錄最高分
+LoadHightestScore();
+document.getElementById("high_score").innerHTML = "最高分數:" + highestScore;
+
 //初始蛇座標
 snake[0] = { x: 80, y: 0 };
 for (let index = 1; index < 4; index++) {
@@ -18,7 +24,7 @@ for (let index = 1; index < 4; index++) {
 }
 //繪製蛇
 function draw() {
-  console.log("drawing...");
+  //console.log("drawing...");
   for (let index = 0; index < snake.length; index++) {
     ctx.fillStyle = "black";
     ctx.fillRect(snake[index].x, snake[index].y, unit, unit); //fillRect(x, y, width, height);
@@ -68,9 +74,23 @@ function move() {
 
   let newHead = { x: s_x, y: s_y };
   snake.unshift(newHead);
-  console.log(newHead);
+  //console.log(newHead);
+
+  //結束判定
+  for (let index = 1; index < snake.length; index++) {
+    if (snake[index].x == s_x && snake[index].y == s_y) {
+      console.log("game over");
+      gameOver();
+      return;
+    }
+  }
+
+  //蘋果判定
   if (s_x == ax && s_y == ay) {
     makeApple();
+    score += 10;
+    setHighScore(score);
+    document.getElementById("game_score").innerHTML = "遊戲分數:" + score;
   } else {
     snake.pop();
   }
@@ -97,10 +117,33 @@ function makeApple() {
     snake.forEach((e) => {
       if (e.x == ax && e.y == ay) {
         k == false;
+        return;
       }
     });
   } while (k == false);
   // console.log(ax, "+", ay);
+}
+
+//game over
+function gameOver() {
+  clearInterval(myGame);
+  alert("遊戲結束 分數: " + score);
+  setHighScore(score);
+}
+
+//最高分
+function LoadHightestScore() {
+  if (Number(localStorage.getItem("highestScore") == null)) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+
+function setHighScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+  }
 }
 
 //區間內隨機整數
@@ -151,5 +194,3 @@ window.addEventListener(
   },
   true
 );
-
-//https://developer.mozilla.org/zh-TW/docs/Web/API/setInterval
